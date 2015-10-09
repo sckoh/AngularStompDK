9d8def19f034200a587f1fc22f88eba5cd0b7d97
soocheng <soocheng@soocheng-MacBook-Pro.local>
2015-10-09 15:31:12 +0800

    modify for chat-internal

--- dist/angular-stomp.js
+++ dist/angular-stomp.js
@@ -1,7 +1,7 @@
 /*! AngularStompDK v0.3.0 */
-(function () {
+(function() {
     'use strict';
-    var _createClass = function () {
+    var _createClass = function() {
         function defineProperties(target, props) {
             for (var key in props) {
                 var prop = props[key];
@@ -11,7 +11,7 @@
             }
             Object.defineProperties(target, props);
         }
-        return function (Constructor, protoProps, staticProps) {
+        return function(Constructor, protoProps, staticProps) {
             if (protoProps)
                 defineProperties(Constructor.prototype, protoProps);
             if (staticProps)
@@ -19,12 +19,12 @@
             return Constructor;
         };
     }();
-    var _classCallCheck = function (instance, Constructor) {
+    var _classCallCheck = function(instance, Constructor) {
         if (!(instance instanceof Constructor)) {
             throw new TypeError('Cannot call a class as a function');
         }
     };
-    var ngstompProvider = function () {
+    var ngstompProvider = function() {
         function ngstompProvider() {
             _classCallCheck(this, ngstompProvider);
             this.settings = {};
@@ -38,15 +38,15 @@
                 }
             },
             url: {
-                value: function (_url) {
+                value: function(_url) {
                     var _urlWrapper = function url(_x) {
                         return _url.apply(this, arguments);
                     };
-                    _urlWrapper.toString = function () {
+                    _urlWrapper.toString = function() {
                         return _url.toString();
                     };
                     return _urlWrapper;
-                }(function (url) {
+                }(function(url) {
                     this.settings.url = url;
                     return this;
                 })
@@ -84,7 +84,7 @@
         });
         return ngstompProvider;
     }();
-    var ngStompWebSocket = function () {
+    var ngStompWebSocket = function() {
         /*@ngNoInject*/
         function ngStompWebSocket(settings, $q, $log, $rootScope, Stomp, $timeout, Session) {
             _classCallCheck(this, ngStompWebSocket);
@@ -100,41 +100,67 @@
             this.promiseResult = this.deferred.promise;
             this.subscribed = [];
             this.Session = Session;
-            this.connect();
         }
         _createClass(ngStompWebSocket, {
             prepareUrlWithToken: {
                 value: function prepareUrlWithToken() {
-                    return this.settings.url + '?access_token=' + this.Session.getSocialBearerToken(true);
+                    var _this = this;
+                    return this.Session.getSocialBearerToken(true)
+                        .then(function(result) {
+                            return _this.settings.url + '?access_token=' + result;
+                        });
                 }
             },
             connect: {
                 value: function connect() {
                     var _this = this;
-                    this.stompClient = this.settings['class'] ? this.Stomp.over(new this.settings['class'](this.prepareUrlWithToken())) : this.Stomp.client(this.prepareUrlWithToken());
-                    this.stompClient.debug = this.settings.debug ? this.$log.debug : function () {
-                    };
-                    this.stompClient.connect({}, function () {
-                        _this.deferred.resolve();
-                        for (var j = 0, len = _this.subscribed.length; j < len; j++) {
-                            _this.subscribed[j].subscribeFn();
-                        }
-                        _this.$digestStompAction();
-                    }, function () {
-                        console.log('fail ws');
-                        _this.$timeout(function () {
-                            _this.connect();
-                        }, _this.settings.RECONNECT_TIMEOUT);
-                        _this.$digestStompAction();
-                    });
-                    return this.promiseResult;
+                    return _this.prepareUrlWithToken()
+                        .then(function(url) {
+                            _this.stompClient = _this.settings['class'] ? _this.Stomp.over(new _this.settings['class'](url)) : _this.Stomp.client(url);
+                            _this.stompClient.debug = _this.settings.debug ? _this.$log.debug : function() {};
+                            _this.stompClient.connect({}, function() {
+                                _this.deferred.resolve();
+                                for (var j = 0, len = _this.subscribed.length; j < len; j++) {
+                                    _this.subscribed[j].subscribeFn();
+                                }
+                                _this.$digestStompAction();
+                            }, function() {
+                                console.log('fail ws');
+                                _this.$timeout(function() {
+                                    _this.connect();
+                                }, _this.settings.RECONNECT_TIMEOUT);
+                                _this.$digestStompAction();
+                            });
+                            return _this.promiseResult;
+                        });
+                    // var url = 'http://172.16.16.32:9000/ws/chat?access_token=5e5b4232-f078-49ff-a411-4f8693c6c911';
+                    // _this.stompClient = _this.settings['class'] ? _this.Stomp.over(new _this.settings['class'](url)) : _this.Stomp.client(url);
+                    // _this.stompClient.debug = _this.settings.debug ? _this.$log.debug : function() {};
+                    // _this.stompClient.connect({}, function() {
+                    //     _this.deferred.resolve();
+                    //     for (var j = 0, len = _this.subscribed.length; j < len; j++) {
+                    //         _this.subscribed[j].subscribeFn();
+                    //     }
+                    //     _this.$digestStompAction();
+                    // }, function() {
+                    //     console.log('fail ws');
+                    //     _this.$timeout(function() {
+                    //         _this.connect();
+                    //     }, _this.settings.RECONNECT_TIMEOUT);
+                    //     _this.$digestStompAction();
+                    // });
+                }
+            },
+            isConnected: {
+                value: function isConnected() {
+                    return this.stompClient && this.stompClient.connected;
                 }
             },
             subscribe: {
                 value: function subscribe(url, callback, header, scope) {
                     var _this = this;
-                    this.promiseResult.then(function () {
-                        var subscribeFn = function () {
+                    this.promiseResult.then(function() {
+                        var subscribeFn = function() {
                             _this.$stompSubscribe(url, callback, header || {});
                             _this.unRegisterScopeOnDestroy(scope, url);
                         };
@@ -150,7 +176,7 @@
             unsubscribe: {
                 value: function unsubscribe(url) {
                     var _this = this;
-                    this.promiseResult.then(function () {
+                    this.promiseResult.then(function() {
                         return _this.$stompUnSubscribe(url);
                     });
                     return this;
@@ -160,7 +186,7 @@
                 value: function send(queue, data, header) {
                     var _this = this;
                     var sendDeffered = this.$q.defer();
-                    this.promiseResult.then(function () {
+                    this.promiseResult.then(function() {
                         _this.stompClient.send(queue, header || {}, JSON.stringify(data));
                         sendDeffered.resolve();
                     });
@@ -171,7 +197,7 @@
                 value: function disconnect() {
                     var _this = this;
                     var disconnectionPromise = this.$q.defer();
-                    this.stompClient.disconnect(function () {
+                    this.stompClient.disconnect(function() {
                         disconnectionPromise.resolve();
                         _this.$digestStompAction();
                     });
@@ -181,7 +207,7 @@
             $stompSubscribe: {
                 value: function $stompSubscribe(queue, callback, header) {
                     var self = this;
-                    var subscription = self.stompClient.subscribe(queue, function () {
+                    var subscription = self.stompClient.subscribe(queue, function() {
                         callback.apply(self.stompClient, arguments);
                         self.$digestStompAction();
                     }, header);
@@ -225,7 +251,7 @@
                 value: function unRegisterScopeOnDestroy(scope, url) {
                     var _this = this;
                     if (scope !== undefined && angular.isFunction(scope.$on))
-                        scope.$on('$destroy', function () {
+                        scope.$on('$destroy', function() {
                             return _this.unsubscribe(url);
                         });
                 }
